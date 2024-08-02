211571ef0b5e79cffe038fd4baec7adf95e4880f
AngelicChicken <yuvelimcheris2003@gmail.com>
2024-08-02 14:15:54 +0700

    add put(edit) function

--- tesVue/src/App.vue
+++ tesVue/src/App.vue
@@ -1,25 +1,38 @@
 <script setup>
-  import {ref} from 'vue'
+  import {ref, onMounted} from 'vue'
   import axios from 'axios';
 
   const todoData = ref([])
   const newTodo = ref('')
+  const editMessage = ref('')
+  const editID = ref()
+  const link = 'https://66aba96d636a4840d7cb82a3.mockapi.io/'
 
   async function fetchData(){
     todoData.value = null
     axios 
-      .get('https://66aba96d636a4840d7cb82a3.mockapi.io/activity')
+      .get(`${link}activity`)
       .then((response)=> {todoData.value = response.data})
   }
 
   async function sendData(){
-    let message = newTodo.value
+    let message =  `${newTodo.value}`
     axios
-      .post('https://66aba96d636a4840d7cb82a3.mockapi.io/activity', message)
+      .post(`${link}activity`, {message})
       .then(fetchData())
   }
 
-  fetchData()
+  async function editData(){
+    let message =  `${editMessage.value}`
+    let id = `${editID.value}`
+    axios
+      .put(`${link}activity/${id}`, {message})
+      .then(fetchData())
+  }
+
+  onMounted(()=>{
+    fetchData()
+  })
 </script>
 
 <template>
@@ -29,5 +42,10 @@
   <form @submit.prevent="sendData">
     <input v-model="newTodo" required placeholder="message">
     <button>add to do</button>
+  </form>
+  <form @submit.prevent="editData">
+    <input v-model="editMessage" required placeholder="message">
+    <input v-model="editID" required placeholder="id">
+    <button>edit</button>
   </form>
 </template>
