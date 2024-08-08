b1fa860db7e1541fe66e22c626a4557d293c700e
AngelicChicken <yuvelimcheris2003@gmail.com>
2024-08-09 01:58:06 +0700

    add bulma, vueRoute

---
+++ tesVue/src/components/ProductList.vue
@@ -0,0 +1,74 @@
+<script setup>
+  import {ref, onMounted, watch} from 'vue'
+  import axios from 'axios';
+
+  const todoData = ref([])
+  const newTodo = ref('')
+  const editMessage = ref('')
+  const editID = ref()
+  const link = 'https://66aba96d636a4840d7cb82a3.mockapi.io/'
+
+  async function fetchData(){
+    todoData.value = null
+    axios 
+      .get(`${link}activity`)
+      .then((response)=> {todoData.value = response.data})
+  }
+
+  async function sendData(){
+    let message =  `${newTodo.value}`
+    axios
+      .post(`${link}activity`, {message})
+      .then(() => fetchData())
+  }
+
+  async function editData(){
+    let message =  `${editMessage.value}`
+    let id = `${editID.value}`
+    axios
+      .put(`${link}activity/${id}`, {message})
+      .then(() => fetchData())
+  }
+
+  async function deleteData(id){
+    axios
+      .delete(`${link}activity/${id}`)
+      .then(() => fetchData())
+  }
+
+  onMounted(()=>{
+    fetchData()
+  })
+
+  watch(todoData.value ,fetchData)
+</script>
+<template>
+    <thead>
+    <tr>
+      <th scope="col">id</th>
+      <th scope="col">message</th>
+      <th scope="col">action</th>
+    </tr>
+    <tr v-for="item of todoData">
+      <th>{{ item.id }}</th>
+      <th>{{ item.message }}</th>
+      <th>
+        <button @click="deleteData(item.id)">delete</button>
+        <router-link 
+            :to="{name: 'Edit', params: {id: item.id}}" 
+            class="button">
+            Edit
+        </router-link>
+      </th>
+    </tr>
+  </thead>
+  <form @submit.prevent="sendData">
+    <input v-model="newTodo" required placeholder="message">
+    <button>add to do</button>
+  </form>
+  <form @submit.prevent="editData">
+    <input v-model="editMessage" required placeholder="message">
+    <input v-model="editID" required placeholder="id">
+    <button>edit</button>
+  </form>
+</template>
--- tesVue/src/main.js
+++ tesVue/src/main.js
@@ -1,6 +1,42 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp, h } from 'vue'
+const app  = createApp({
+    render: ()=>h(App)
+});
 
-createApp(App).mount('#app')
+import * as VueRouter from 'vue-router'
+
+import App from './App.vue'
+import Create from './components/ProductAdd.vue'
+import Edit from './components/ProductEdit.vue'
+import Index from './components/ProductList.vue'
+
+app.use(VueRouter)
+
+app.config.productionTip = false
+
+const routes = [
+    {
+        name: 'Create',
+        path: '/create',
+        component: Create 
+    },
+    {
+        name: 'Edit',
+        path: '/edit/:id',
+        component: Edit
+    },
+    {
+        name: 'Index',
+        path: '/',
+        component: Index
+    },
+];
+
+const router = VueRouter.createRouter({
+    history: VueRouter.createWebHashHistory(), 
+    routes: routes})
+
+app.use(router)
+app.mount('#app')
